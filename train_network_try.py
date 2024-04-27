772dcc1c8c1f76712cce06062e546e3b07ef8d75
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-04-27 09:52:14 +0800

    Update train_network_try.py

--- train_network_try.py
+++ train_network_try.py
@@ -856,7 +856,7 @@ class NetworkTrainer:
                         pos_latents = self.process_latents(pos_batch, vae, vae_dtype, weight_dtype, vae_scale_factor)
                         print("test_posivate_batch",pos_batch)
                         print("test_batch",batch)
-                        pos_text_encoder_conds = self.get_text_embedding(tokenizer, text_encoder, pos_batch, accelerator,args, train_text_encoder, args.clip_skip, weight_dtype)
+                        pos_text_encoder_conds = self.get_text_embedding(tokenizers, text_encoder, pos_batch, accelerator,args, train_text_encoder, args.clip_skip, weight_dtype)
                         pos_noise, pos_noisy_latents, pos_timesteps, pos_huber_c = train_util.get_noise_noisy_latents_and_timesteps(
                             args, noise_scheduler, pos_latents
                         )
@@ -895,7 +895,7 @@ class NetworkTrainer:
                             raise NotImplementedError("multipliers for each sample is not supported yet")
                         # print(f"set multiplier: {multipliers}")
                         accelerator.unwrap_model(network).set_multiplier(multipliers)
-                    text_encoder_conds = self.get_text_embedding(tokenizer, text_encoder, batch, accelerator,args, train_text_encoder, args.clip_skip, weight_dtype)
+                    text_encoder_conds = self.get_text_embedding(tokenizers, text_encoder, batch, accelerator,args, train_text_encoder, args.clip_skip, weight_dtype)
                     
                     # Sample noise, sample a random timestep for each image, and add noise to the latents,
                     # with noise offset and/or multires noise if specified
