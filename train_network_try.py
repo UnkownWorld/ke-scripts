7cb7201f496b2af2caa0d78e8a71af262f194a85
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-04-27 09:19:49 +0800

    Update train_network_try.py

--- train_network_try.py
+++ train_network_try.py
@@ -848,11 +848,11 @@ class NetworkTrainer:
                 with accelerator.accumulate(training_model):
                     on_step_start(text_encoder, unet)
 
-                    latents = self.process_latents(batch, vae, vae_dtype, weight_dtype, vae_scale_factor)
+                    latents = self.process_latents(batch, vae, vae_dtype, weight_dtype, self.vae_scale_factor)
                     pos_batch = train_dataloader.dataset[random.randint(0, num_update_steps_per_epoch-1)]
                     if(step in positivate_steps):
                         is_posivate = True
-                        pos_latents = self.process_latents(pos_batch, vae, vae_dtype, weight_dtype, vae_scale_factor)
+                        pos_latents = self.process_latents(pos_batch, vae, vae_dtype, weight_dtype, self.vae_scale_factor)
                         pos_text_encoder_conds = self.get_text_embedding(tokenizer, text_encoder, pos_batch, accelerator, args, args.clip_skip, weight_dtype)
                         pos_noise, pos_noisy_latents, pos_timesteps, pos_huber_c = train_util.get_noise_noisy_latents_and_timesteps(
                             args, noise_scheduler, pos_latents
