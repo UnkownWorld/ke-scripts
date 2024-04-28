cb2f6f6a8d6736f75fd2ea34fe45bb22b475b995
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-04-29 01:07:46 +0800

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -130,7 +130,7 @@ except:
 IMAGE_TRANSFORMS = transforms.Compose(
     [
         transforms.ToTensor(),
-        transforms.Normalize([0.5], [0.5]),
+        transforms.Normalize([0], [1]),
     ]
 )
 
