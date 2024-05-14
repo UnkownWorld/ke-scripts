7d9221e4bc955a5aaebdf25489920c829bdb9f0b
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-05-14 21:14:57 +0800

    Update custom_train_functions.py

--- library/custom_train_functions.py
+++ library/custom_train_functions.py
@@ -522,15 +522,15 @@ def apply_noise_for_peil(latents,noise):
     logger.info(f"fix noise {noise}")
     # 生成泊松噪声
     poisson_noise = torch.poisson(torch.ones((batch_size, channels, height, width), device=device))
-    logger.info(f"fix combined_noise1 {poisson_noise}")
+    #logger.info(f"fix combined_noise1 {poisson_noise}")
     # 生成蓝噪声
     blue_noise = generate_fractal_noise(batch_size, channels, height, width, latents, fractal_type='wiener')
-    logger.info(f"fix combined_noise1 {blue_noise}")
+    #logger.info(f"fix combined_noise1 {blue_noise}")
     # 生成红噪声
     red_noise = generate_fractal_noise(batch_size, channels, height, width, latents, fractal_type='brownian')
-    logger.info(f"fix combined_noise1 {red_noise}")
+    #logger.info(f"fix combined_noise1 {red_noise}")
     # 将三种噪声相加并返回
-    combined_noise = noise + poisson_noise + blue_noise + red_noise
+    combined_noise = 0.6 * noise + 0.2 * poisson_noise + 0.1 * blue_noise + 0.1 * red_noise
     logger.info(f"fix combined_noise {combined_noise}")
     return combined_noise
 
