363db3de93724755558edb76585f415ee0390c85
UnkownWorld <49099528+UnkownWorld@users.noreply.github.com>
2024-05-19 20:25:33 +0800

    Update original_unet.py

--- library/original_unet.py
+++ library/original_unet.py
@@ -1674,9 +1674,9 @@ class UNet2DConditionModel(nn.Module):
         sample = self.mid_block(sample, emb, encoder_hidden_states=encoder_hidden_states)
         # Add SPP layer
         #logger.info(f"step_testforit:{self.current_step},pool_current_weight_test, {self.pool_current_weight}")
-        #sample = sample * self.pool_current_weight[0] + self.add_spp_layer(sample) * self.pool_current_weight[1] + self.addmaxpool(sample) * self.pool_current_weight[2]
         if is_sample  == False:
-            sample = sample * 0.3 + self.add_spp_layer(sample) * 0.3 + self.addmaxpool(sample) * 0.2
+            #sample = sample * 0.3 + self.add_spp_layer(sample) * 0.3 + self.addmaxpool(sample) * 0.2
+            sample = sample * self.pool_current_weight[0] + self.add_spp_layer(sample) * self.pool_current_weight[1] + self.addmaxpool(sample) * self.pool_current_weight[2]
         # ControlNetの出力を追加する
         if mid_block_additional_residual is not None:
             sample += mid_block_additional_residual
